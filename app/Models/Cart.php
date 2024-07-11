5fcf9f4ad0db5a8f1a1f3712f3667a6ad95ec5bd
Mohamed Saad <131921827+Mohamed-Saad288@users.noreply.github.com>
2024-07-11 21:09:38 +0300

    create cart migration , create observer to assign value of uuid , create relation between cart and user , product

---
+++ app/Models/Cart.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use App\Observers\CartObserver;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    public $incrementing = false;
+
+    protected $fillable = [
+        'cookie_id',
+        'user_id',
+        'product_id',
+        'quantity',
+        'options'
+    ];
+
+    public static function booted()
+    {
+//        static::creating(function (Cart $cart){
+//            $cart->id = Str::uuid();
+//        });
+        static::observe(CartObserver::class);
+    }
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)
+            ->withDefault([
+                'name' => 'Anonymous'
+            ]);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
