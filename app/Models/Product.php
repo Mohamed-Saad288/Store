cf666b5c9210381746b215de3fdc3a12b2266818
Mohamed Saad <131921827+Mohamed-Saad288@users.noreply.github.com>
2024-07-11 16:56:51 +0300

    Create Home View With Products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -27,7 +28,7 @@ class Product extends Model
     {
         static::addGlobalScope('store',function (Builder $builder){
            $user = Auth::user();
-           if ($user->store_id)
+           if ($user && $user->store_id)
            {
                $builder->where('store_id','=',$user->store_id);
            }
@@ -59,5 +60,31 @@ class Product extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class,'product_tag','product_id','tag_id','id','id');
+    }
+    public function scopeActive(Builder $builder)
+    {
+        $builder->where('status','=','active');
+    }
+
+    // Accessors
+    public function getImageUrlAttribute()
+    {
+        if (!$this->image) {
+            return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKnKnw0MtmVH5_-A-wrEh5OiTSL3lu_5MZZA&s';
+        }
+        if (Str::startsWith($this->image, ['http://', 'https://'])) {
+            return $this->image;
+
+        }
+        return asset('storage/' . $this->image);
+
+    }
+    public function getSalePercentAttribute()
+    {
+        if (!$this->compare_price)
+        {
+            return 0 ;
+        }
+        return  round(100 - (100 * $this->price / $this->compare_price),1 );
     }
 }
